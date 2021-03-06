1a492e15dc9e79599dbe64eb3d2905fb4b2eed3b
VitoSenic <vito.senic@gmail.com>
2021-03-06 19:33:13 +0100

    employerscard

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,3 +3,9 @@ const card = document.querySelector(".card__inner");
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
 });
+
+$(function() {
+	$(".block").on("click", function() {
+		$(".info").toggleClass("active");
+	});
+});
