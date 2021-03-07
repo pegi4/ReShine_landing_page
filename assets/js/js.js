79df57cf5c140efe854b25cd4fb477f7483a25d0
VitoSenic <vito.senic@gmail.com>
2021-03-07 10:08:33 +0100

    fix card

--- assets/js/js.js
+++ assets/js/js.js
@@ -8,4 +8,13 @@ $(function() {
 	$(".block").on("click", function() {
 		$(".info").toggleClass("active");
 	});
+	$(".bblock").on("click", function() {
+		$(".iinfo").toggleClass("active");
+	});
+	$(".bbblock").on("click", function() {
+		$(".iiinfo").toggleClass("active");
+	});
+	$(".bbbblock").on("click", function() {
+		$(".iiiinfo").toggleClass("active");
+	});
 });
