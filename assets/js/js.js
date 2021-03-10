223b3e1cd7d9e0ea8035e2d7b30f652d2fdf1c6a
VitoSenic <vito.senic@gmail.com>
2021-03-10 06:31:11 +0100

    js. header fixes

--- assets/js/js.js
+++ assets/js/js.js
@@ -19,14 +19,49 @@ $(function() {
 	});
 });
 
+/*
 $(document).ready(function() {
 	$(window).scroll(function() {
         console.log('scrolling ', $(window).scrollTop(), $(document).height());
     	if ($(window).scrollTop() >= 700 && $(window).scrollTop() <= ($(document).height() - 700)) {
-        	$('header').removeClass('hide');
-        }
-        else {
         	$('header').addClass('hide');
         }
+        else {
+        	$('header').removeClass('hide');
+        }
     });
-});
+});*/
+
+function myFunction(x) {
+	if (x.matches) { // If media query matches
+		document.getElementById("hlogo").style.height = "5rem";
+		$(document).ready(function() {
+			$(window).scroll(function() {
+				console.log('scrolling ', $(window).scrollTop(), $(document).height());
+				if ($(window).scrollTop() >= 700 && $(window).scrollTop() <= ($(document).height() - 700)) {
+					$('header').addClass('hide');
+				}
+				else {
+					$('header').removeClass('hide');
+				}
+			});
+		});
+	} else {
+		document.getElementById("hlogo").style.height = "3rem";
+		$(document).ready(function() {
+			$(window).scroll(function() {
+				console.log('scrolling ', $(window).scrollTop(), $(document).height());
+				if ($(window).scrollTop() >= 500 && $(window).scrollTop() <= ($(document).height() - 400)) {
+					$('header').addClass('hide');
+				}
+				else {
+					$('header').removeClass('hide');
+				}
+			});
+		});
+	}
+  }
+  
+  var x = window.matchMedia("(min-width: 700px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
