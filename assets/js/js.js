67d8ca05fccdf71c8a5a88809cd2004fa3ce432d
VitoSenic <vito.senic@gmail.com>
2021-03-18 11:41:02 +0100

    product

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,42 +1,4 @@
 /*--------------------------------------------
-*                   Info button
-----------------------------------------------*/
-
-const card = document.querySelector(".card__inner");
-
-card.addEventListener("click", function (e) {
-  card.classList.toggle('is-flipped');
-});
-
-$(function() {
-	$(".block").on("click", function() {
-		$(".info").toggleClass("active");
-	});
-	$(".bblock").on("click", function() {
-		$(".iinfo").toggleClass("active");
-	});
-	$(".bbblock").on("click", function() {
-		$(".iiinfo").toggleClass("active");
-	});
-	$(".bbbblock").on("click", function() {
-		$(".iiiinfo").toggleClass("active");
-	});
-});
-
-/* Add class when scrolled 700px
-$(document).ready(function() {
-	$(window).scroll(function() {
-        console.log('scrolling ', $(window).scrollTop(), $(document).height());
-    	if ($(window).scrollTop() >= 700 && $(window).scrollTop() <= ($(document).height() - 700)) {
-        	$('header').addClass('hide');
-        }
-        else {
-        	$('header').removeClass('hide');
-        }
-    });
-});*/
-
-/*--------------------------------------------
 *       On scroll show header logo
 ----------------------------------------------*/
 
@@ -68,12 +30,51 @@ function myFunction(x) {
 			});
 		});
 	}
-  }
+}
+
+var x = window.matchMedia("(min-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+/*--------------------------------------------
+*                   Info button
+----------------------------------------------*/
+
+const card = document.querySelector(".card__inner");
+
+$(function() {
+	$(".block").on("click", function() {
+		$(".info").toggleClass("active");
+	});
+	$(".bblock").on("click", function() {
+		$(".iinfo").toggleClass("active");
+	});
+	$(".bbblock").on("click", function() {
+		$(".iiinfo").toggleClass("active");
+	});
+	$(".bbbblock").on("click", function() {
+		$(".iiiinfo").toggleClass("active");
+	});
+});
 
 window.onload = function(){
 	document.getElementById('ll').style.opacity = 1;
 	document.getElementById('ll').style.marginTop = "0rem";
 	document.getElementById('opis').style.opacity = 1;
 }
+
+/* Add class when scrolled 700px
+$(document).ready(function() {
+	$(window).scroll(function() {
+        console.log('scrolling ', $(window).scrollTop(), $(document).height());
+    	if ($(window).scrollTop() >= 700 && $(window).scrollTop() <= ($(document).height() - 700)) {
+        	$('header').addClass('hide');
+        }
+        else {
+        	$('header').removeClass('hide');
+        }
+    });
+});*/
+
 
 
