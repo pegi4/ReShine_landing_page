aa1e9979f83774da2d4153862d78b90bedb17dce
VitoSenic <vito.senic@gmail.com>
2021-04-11 09:54:55 +0200

    slider

--- assets/js/js.js
+++ assets/js/js.js
@@ -76,5 +76,14 @@ $(document).ready(function() {
     });
 });*/
 
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 5000);
+
 
 
